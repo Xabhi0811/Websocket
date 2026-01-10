b3bd0a74d45c3a05b56c10070e26e228ebb9de78
Abhishek Singh Chauhan <cbittu349@gmail.com>
2026-01-10 19:31:30 +0530

    frontend added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+ import React from 'react'
+ import './App.css'
+ import { Route, BrowserRouter, Routes } from 'react-router-dom'
+ import {Sender} from './Components/Sender'
+ import {Receiver} from './Components/Receiver'
+ 
+ const App = () => {
+   return (
+     <BrowserRouter>
+     <Routes>
+      <Route path="/sender" element={<Sender />} />
+      <Route path="/receiver" element={<Receiver />} />
+     </Routes>
+     </BrowserRouter>
+   )
+ }
+ 
+ export default App
+ 
---
+++ frontend/src/Components/Sender.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sender = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sender
